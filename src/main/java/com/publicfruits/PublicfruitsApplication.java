21f87b68643eb0616d4501a7b922f1e53d13d019
Pedro Ventura <pedro.asventura@gmail.com>
2023-11-05 21:30:25 +0100

    Project bootstrap

---
+++ src/main/java/com/publicfruits/PublicfruitsApplication.java
@@ -0,0 +1,13 @@
+package com.publicfruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicfruitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicfruitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicfruits/PublicfruitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicfruits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicfruitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
